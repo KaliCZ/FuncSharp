622fbfb2f173b37c8aedadc503a9d2d435a3d733
KaliCZ <kalandrapavel@gmail.com>
2025-11-29 00:05:38 +0100

    Also extensions for nullable objects.

---
+++ src/FuncSharp/Option/ObjectExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+
+namespace FuncSharp;
+
+public static partial class OptionExtensions
+{
+    /// <summary>
+    /// Maps the not null value using the specified function and returns the result.
+    /// </summary>
+    [Pure]
+    public static TResult? Map<T, TResult>(this T? value, Func<T, TResult> func)
+        where T : struct
+    {
+        if (value is {} v)
+        {
+            return func(v);
+        }
+
+        return default;
+    }
+
+    /// <summary>
+    /// Maps the not null value using the specified function and returns the result.
+    /// </summary>
+    [Pure]
+    public static TResult? Map<T, TResult>(this T? value, Func<T, TResult> func)
+        where T : class
+    {
+        if (value is {} v)
+        {
+            return func(v);
+        }
+
+        return default;
+    }
+
+    /// <summary>
+    /// Maps the not null value using the specified function and returns the result.
+    /// </summary>
+    [Pure]
+    public static async Task<TResult?> MapAsync<T, TResult>(this T? value, Func<T, Task<TResult>> func)
+        where T : struct
+    {
+        if (value is {} v)
+        {
+            return await func(v);
+        }
+
+        return default;
+    }
+
+    /// <summary>
+    /// Maps the not null value using the specified function and returns the result.
+    /// </summary>
+    [Pure]
+    public static async Task<TResult?> MapAsync<T, TResult>(this T? value, Func<T, Task<TResult>> func)
+        where T : class
+    {
+        if (value is {} v)
+        {
+            return await func(v);
+        }
+
+        return default;
+    }
+}
